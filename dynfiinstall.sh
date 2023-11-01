15b5d3bfd7d157a0ed6b5475e25bba59bff43636
shley008 <61912791+shley008@users.noreply.github.com>
2023-10-31 22:20:45 -0500

    update version

--- dynfiinstall.sh
+++ dynfiinstall.sh
@@ -2,4 +2,4 @@
 echo "Installing and launching ${BLUE}DynFi Manager${NC}..."
 DFMver=$(curl "https://dynfi.com/latest-version")
 DFver=$($DFMVer | sed 's/\r//g')
-wget "https://dynfi.com/files/dynfi/dynfi-23.1.1-all.jar" -O /app/dynfi.jar
+wget "https://dynfi.com/files/dynfi/dynfi-23.2.2-all.jar" -O /app/dynfi.jar
